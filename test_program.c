8dda8092c9ac64f6591bf21ce5709cdf84888997
prashant <prashant.kuls@gmail.com>
2025-07-23 08:13:21 +0000

    COMMIT

---
+++ test_program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Prompt user for the first number
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    // Prompt user for the second number
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Add the two numbers
+    sum = num1 + num2;
+
+    // Display the result
+    printf("Sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
+
