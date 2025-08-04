643456cc369c7f613c2bf21a417ee1fdd92c64db
Prashant Kumar <prashant.kuls@gmail.com>
2025-08-04 08:12:25 +0000

    corrected the code error - checking jenkins job

--- test_program.c
+++ test_program.c
@@ -22,7 +22,8 @@ int main() {
     printf("commit on 03-aug\n");
     printf("commit to test jenkins\n");
     printf("testing again as on Aug 04, 00:22\n");
-    printf("valication before demo\n")
+    printf("valication before demo\n");
+    
     return 0;
 }
 
